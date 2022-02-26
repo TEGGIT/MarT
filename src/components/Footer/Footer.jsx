3c187ef4b9582e6a88df3292b4be656ec50da644
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-02-26 16:28:05 +0300

    Create Footer
    Final version page MarT

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,14 @@ const Footer = () => {
   return (
       <footer className={classes.footer}>
         <div className={classes.footer__nav}>
+          <div className={classes.footer__nav__logo}>
           <img src={logo} alt="логотип"/>
+        </div>
+          <div className={classes.footer__nav__text}>
+            <p>
+              © 2022  |  Privacy Policy  |  made by a team of people who, unfortunately, were not taken into an honest company, as you can see, in vain
+              </p>
+          </div>
         </div>
       </footer>
   );
