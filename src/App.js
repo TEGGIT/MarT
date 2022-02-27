aa94ee1065ad3af064a49435d9b24bb0c6acd939
Fron <syper.martunenko200@yandex.ru>
2022-02-28 02:28:40 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 
 import './App.css';
-import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
-import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contact";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import {Route, Routes, BrowserRouter} from "react-router-dom";
 
 
 function App() {
-  return (
-      <div className="App">
+    return (
+        <div className="App">
 
-  <BrowserRouter>
-        <Routes>
-           <Route path="/" element={<MainContent />} />
-           <Route path="/main-page" element={<MainContent/>} />
-      <Route  path="/contact-page" element={<Contact/>}/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainContent/>}/>
+                    <Route path="/main-page" element={<MainContent/>}/>
+                    <Route path="/contact-page" element={<Contact/>}/>
 
-        </Routes>
-      </BrowserRouter>
-      </div>
-  );
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
