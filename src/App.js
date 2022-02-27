d511707f69c6b7d1c9e9924d0dc095c4b38350ff
Fron <syper.martunenko200@yandex.ru>
2022-02-28 00:54:39 +0300

    Create contact page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
      <Header/>
-      <MainContent/>
+        <Contact/>
+     {/* <MainContent/>*/}
       <Footer/>
+
+
+
     </div>
   );
 }
