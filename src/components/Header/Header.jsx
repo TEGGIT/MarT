5e6161fb3dcd32f3fe53eaf7479f32171b8412e8
Fron <syper.martunenko200@yandex.ru>
2022-03-01 16:15:36 +0300

    I added an animation of underlining nav items when pointing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,45 +4,45 @@ import logo from "../../assets/MarT.svg"
 import iconN from "../../assets/nomera.svg"
 import iconA from "../../assets/Uslugi.svg"
 import iconP from "../../assets/Phone.svg"
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
-  return (
-      <header className={classes.header}>
-       <nav className={classes.header__nav}>
-         <NavLink to="/main-page" className={classes.header__nav_link}>
-      <img src={logo} alt={logo}/>
-         </NavLink>
-         <div className={classes.header__nav__btns__container}>
+    return (
+        <header className={classes.header}>
+            <nav className={classes.header__nav}>
+                <NavLink to="/main-page" className={classes.header__nav_link}>
+                    <img src={logo} alt={logo}/>
+                </NavLink>
+                <div className={classes.header__nav__btns__container}>
 
-           <div className={classes.header__nav__btns__container_gap}>
-            <img src={iconN} alt="номера"/>
-           <p>
-             Номера
-           </p>
-         </div>
+                    <div className={classes.header__nav__btns__container_gap}>
+                        <img src={iconN} alt="номера"/>
+                        <p>
+                            Номера
+                        </p>
+                    </div>
 
-             <NavLink to="/sosi" className={classes.header__nav_link}>
-           <div className={classes.header__nav__btns__container_gap}>
-           <img src={iconA} alt="Услуги"/>
-           <p>
-             Услуги
-           </p>
-           </div>
-             </NavLink>
-           <NavLink to="/contact-page" className={classes.header__nav_link}>
-           <div className={classes.header__nav__btns__container_gap}>
-           <img src={iconP} alt="Контакты"/>
-           <p>
-             Контакты
-           </p>
+                    <NavLink to="/sosi" className={classes.header__nav_link}>
+                        <div className={classes.header__nav__btns__container_gap}>
+                            <img src={iconA} alt="Услуги"/>
+                            <p>
+                                Услуги
+                            </p>
+                        </div>
+                    </NavLink>
+                    <NavLink to="/contact-page" className={classes.header__nav_link}>
+                        <div className={classes.header__nav__btns__container_gap}>
+                            <img src={iconP} alt="Контакты"/>
+                            <p>
+                                Контакты
+                            </p>
 
-           </div>
-           </NavLink>
-         </div>
-       </nav>
-      </header>
-  );
+                        </div>
+                    </NavLink>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
