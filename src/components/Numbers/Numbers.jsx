194b6549767c7ee814407933bc8081c46c316009
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-03-01 10:23:19 +0300

    Fix footer
    Add image page numbers

--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import classes from "./Numbers.module.scss"
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import mainImage from "../../assets/BackNumbers.png"
+
 const Numbers = () => {
   return (
-      <div className={classes.main}>
-hhhhhh
-      </div>
+      <>
+        <Header/>
+      <main className={classes.main}>
+        <section className={classes.main__section}>
+        <div className={classes.main__section__contentTop} style={{backgroundImage: `url(${mainImage})`}}>
+
+        </div>
+        </section>
+      </main>
+        <Footer/>
+      </>
   );
 };
 
