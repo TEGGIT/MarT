d33ad716300ff7f42e5876f6a8cebf347252c8cb
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-03-01 11:34:20 +0300

    Create nav panel filter

--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -11,7 +11,20 @@ const Numbers = () => {
       <main className={classes.main}>
         <section className={classes.main__section}>
         <div className={classes.main__section__contentTop} style={{backgroundImage: `url(${mainImage})`}}>
+          <div className={classes.main__section__contentTop__container}>
+          <div className={classes.main__section__contentTop__container__title}>
+         <h1>Номера</h1>
+        </div>
 
+            <nav className={classes.main__section__contentTop__container__nav}>
+             <ul>
+               <li>All</li>
+               <li >Family room</li>
+               <li >Delux</li>
+               <li >Presidential Suite</li>
+             </ul>
+            </nav>
+        </div>
         </div>
         </section>
       </main>
