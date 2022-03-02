d0e1a2428b7905252d5a934fa925feaa8fec8f80
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-03-02 21:02:13 +0300

    Add card hostel

--- src/components/CardHostel/CardHostel.jsx
+++ src/components/CardHostel/CardHostel.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import classes from "../Numbers/Numbers.module.scss";
-import imageOne from "../../assets/imageNumbers1.png";
 import iconOne from "../../assets/IconNumbers1.svg";
 import iconTwo from "../../assets/iconNumbers2.svg";
 import iconThree from "../../assets/iconNumbers3.svg";
 import iconFour from "../../assets/iconNumbers4.svg";
 import {NavLink} from "react-router-dom";
 
-const CardHostel = () => {
+const CardHostel = ({category, size, places,image,food,info,price}) => {
   return (
       <>
         <div className={classes.main__section__content__container}>
           <div className={classes.main__section__content__container__card}>
 
             <div className={classes.main__section__content__container__card__image}>
-              <img src={imageOne} alt="Изображение номера"/>
+              <img src={image} alt="Изображение номера"/>
 
 
             </div>
             <div className={classes.main__section__content__container__card__info}>
               <div className={classes.main__section__content__container__card__info__text_top}>
                 <p>
-                  Семейный номер
+                  {category}
                 </p>
 
               </div>
@@ -29,7 +28,7 @@ const CardHostel = () => {
 
                 <img src={iconOne} alt="Иконка"/>
                 <p>
-                  175 м2
+                  {size}
                 </p>
 
 
@@ -38,7 +37,7 @@ const CardHostel = () => {
 
                 <img src={iconTwo} alt="Иконка"/>
                 <p>
-                  До 6‑ти человек (также можно разместить до 3‑х детей до 3‑х лет включительно)
+                  {places}
                 </p>
 
 
@@ -47,7 +46,7 @@ const CardHostel = () => {
 
                 <img src={iconThree} alt="Иконкa"/>
                 <p>
-                  Тип питания определяется в зависимости от тарифа
+                  {food}
                 </p>
 
 
@@ -56,13 +55,14 @@ const CardHostel = () => {
 
                 <img src={iconFour} alt="Иконка"/>
                 <p>
-                  Возможно проживание с животными весом не более 5 кг за дополнительную плату </p>
+                  {info}
+                </p>
 
 
               </div>
 
               <div className={classes.main__section__content__container__card__info__status}>
-                <p>30.000₽/сутки</p>
+                <p>{price}</p>
                 <p><span
                     className={classes.main__section__content__container__card__info__status_color}>СВОБОДНО</span>
                 </p>
