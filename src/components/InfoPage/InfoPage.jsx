14f8f6558b7169f2c3ca9a37fe840f6f306b3d8b
Fron <syper.martunenko200@yandex.ru>
2022-03-12 20:01:52 +0300

    Create modal window

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./InfoPage.module.scss";
 import BackImgDescription from '../../assets/BackImgDescription.png'
 import Cloud from '../../assets/Cloud.svg'
@@ -28,141 +28,146 @@ import ExclamationMark from '../../assets/ExclamationMark.svg'
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import {NavLink} from "react-router-dom";
+import Modal from "../Modal/Modal";
 
 const InfoPage = () => {
-    return (
-        <>
-            <Header/>
-            <main className={classes.more}>
-                <div className={classes.nav}>
-                    <p><NavLink to="/numbers-page" className={classes.nav__text}>Номера</NavLink> > Подробнее</p>
-                </div>
-                <div className={classes.container}>
+  const [openModal, setOpenModal] = useState(false)
 
-                    <div className={classes.container__description}
-                         style={{backgroundImage: `url(${BackImgDescription})`}}>
-                        <div className={classes.container__description_info}>
-                            <img src={Cloud} alt={'Иконка воздуха'}/>
-                            <span>Центральная система кондиционирования</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={TV} alt={'Телевизор'}/>
-                            <span>LCD телевизоры</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Lamp} alt={'Лампа'}/>
-                            <span>Настольные лампы</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Bolt} alt={'Электричество'}/>
-                            <span>Фен, Электрочайник, Утюг</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Glass} alt={'Стакан'}/>
-                            <span>Чайная станция: чайные пары, чай, кофе, сахар</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Money} alt={'Деньги'}/>
-                            <span>Сейф</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Bathtub} alt={'Ванна'}/>
-                            <span>Ванная комната с душем и тропическим душем</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Mirror} alt={'Зеркало'}/>
-                            <span>Косметическое зеркало</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Call} alt={'Телефон'}/>
-                            <span>Телефон</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={WiFi} alt={'Wi-Fi'}/>
-                            <span>Высокоскоростнoй WI-FI</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={IroningBoard} alt={'Гладильная доска'}/>
-                            <span>Гладильная доска</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Scales} alt={'Весы'}/>
-                            <span>Весы</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={ToothKit} alt={'Зубной набор'}/>
-                            <span>Зубной набор</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Pillow} alt={'Меню подушек'}/>
-                            <span>Меню подушек</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Bed} alt={'Кровать'}/>
-                            <span>Дополнительная кровать</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Mocktail} alt={'Мини-бар'}/>
-                            <span>Мини-бар*</span>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Balcony} alt={'Балкон'}/>
-                            <span>Балкон</span>
-                        </div>
-                        <div className={classes.container__description_info_line}>
-                        </div>
-                        <div className={classes.container__description_info}>
-                            <img src={Bar} alt={'Балкон'}/>
-                            <span>Услуга мини-бар доступна при наличии депозита</span>
-                        </div>
-                    </div>
-                    <div className={classes.container__reservationCard}>
-                        <img src={ImgNumber} alt={'Картинка номера'}/>
-                        <div className={classes.container__reservationCard_info}>
-                            <div className={classes.container__reservationCard_info_price}>
-                                <span>30.000 </span>
-                                <span>₽/сутки</span>
-                            </div>
-                            <div className={classes.container__reservationCard_info__description}>
-                                <img src={IconSquare} alt={'Иконка маштаба'}/>
-                                <span>175м<sup>2</sup></span>
-                            </div>
-                            <div className={classes.container__reservationCard_info__description}>
-                                <img src={User} alt={'Иконка людей'}/>
-                                <span>До 6‑ти человек (также можно разместить до 3‑х детей до 3‑х лет включительнo)</span>
-                            </div>
-                            <div className={classes.container__reservationCard_info__description}>
-                                <img src={Fork} alt={'Иконка людей'}/>
-                                <span>Тип питания определяется в зависимости от тарифа</span>
-                            </div>
-                            <div className={classes.container__reservationCard_info__description}>
-                                <img src={ExclamationMark} alt={'Иконка людей'}/>
-                                <span>Возможно проживание с животными весом не более 5 кг за дополнительную плату</span>
-                            </div>
-                            <div className={classes.container__reservationCard_info_btn}>
-                                <p>Бронировать</p>
-                            </div>
-                        </div>
-                        <div className={classes.container__reservationCard__description}>
-                            <div className={classes.container__reservationCard__description__title}>
-                                <h1>О номере</h1>
-                            </div>
-                            <div className={classes.container__reservationCard__description_text}>
-                                В красивом интерьере Семейного люкса мы воплотили атмосферу домашнего уюта, сохранив
-                                функциональность и комфорт премиального гостиничного номера. Современный дизайн спальни
-                                и гостиной продуман в деталях — здесь все необходимое для общения и уединенного отдыха с
-                                семьей. Фантастические панорамы природы из окон вашего номера, утонченный сервис и
-                                невероятная территория комплекса подарят вам незабываемые эмоции от пребывания в отеле
-                                MarT
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+      <>
+        <Header/>
+        <main className={classes.more}>
+          <div className={classes.nav}>
+            <p><NavLink to="/numbers-page" className={classes.nav__text}>Номера</NavLink> > Подробнее</p>
+          </div>
+          <div className={classes.container}>
 
-            </main>
-            <Footer/>
-        </>
-    );
+            <div className={classes.container__description}
+                 style={{backgroundImage: `url(${BackImgDescription})`}}>
+              <div className={classes.container__description_info}>
+                <img src={Cloud} alt={'Иконка воздуха'}/>
+                <span>Центральная система кондиционирования</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={TV} alt={'Телевизор'}/>
+                <span>LCD телевизоры</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Lamp} alt={'Лампа'}/>
+                <span>Настольные лампы</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Bolt} alt={'Электричество'}/>
+                <span>Фен, Электрочайник, Утюг</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Glass} alt={'Стакан'}/>
+                <span>Чайная станция: чайные пары, чай, кофе, сахар</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Money} alt={'Деньги'}/>
+                <span>Сейф</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Bathtub} alt={'Ванна'}/>
+                <span>Ванная комната с душем и тропическим душем</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Mirror} alt={'Зеркало'}/>
+                <span>Косметическое зеркало</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Call} alt={'Телефон'}/>
+                <span>Телефон</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={WiFi} alt={'Wi-Fi'}/>
+                <span>Высокоскоростнoй WI-FI</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={IroningBoard} alt={'Гладильная доска'}/>
+                <span>Гладильная доска</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Scales} alt={'Весы'}/>
+                <span>Весы</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={ToothKit} alt={'Зубной набор'}/>
+                <span>Зубной набор</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Pillow} alt={'Меню подушек'}/>
+                <span>Меню подушек</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Bed} alt={'Кровать'}/>
+                <span>Дополнительная кровать</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Mocktail} alt={'Мини-бар'}/>
+                <span>Мини-бар*</span>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Balcony} alt={'Балкон'}/>
+                <span>Балкон</span>
+              </div>
+              <div className={classes.container__description_info_line}>
+              </div>
+              <div className={classes.container__description_info}>
+                <img src={Bar} alt={'Балкон'}/>
+                <span>Услуга мини-бар доступна при наличии депозита</span>
+              </div>
+            </div>
+            <div className={classes.container__reservationCard}>
+              <img src={ImgNumber} alt={'Картинка номера'}/>
+              <div className={classes.container__reservationCard_info}>
+                <div className={classes.container__reservationCard_info_price}>
+                  <span>30.000 </span>
+                  <span>₽/сутки</span>
+                </div>
+                <div className={classes.container__reservationCard_info__description}>
+                  <img src={IconSquare} alt={'Иконка маштаба'}/>
+                  <span>175м<sup>2</sup></span>
+                </div>
+                <div className={classes.container__reservationCard_info__description}>
+                  <img src={User} alt={'Иконка людей'}/>
+                  <span>До 6‑ти человек (также можно разместить до 3‑х детей до 3‑х лет включительнo)</span>
+                </div>
+                <div className={classes.container__reservationCard_info__description}>
+                  <img src={Fork} alt={'Иконка людей'}/>
+                  <span>Тип питания определяется в зависимости от тарифа</span>
+                </div>
+                <div className={classes.container__reservationCard_info__description}>
+                  <img src={ExclamationMark} alt={'Иконка людей'}/>
+                  <span>Возможно проживание с животными весом не более 5 кг за дополнительную плату</span>
+                </div>
+                <button className={classes.container__reservationCard_info_btn} onClick={() => setOpenModal(true)}>
+                  Бронировать
+                </button>
+              </div>
+              <div className={classes.container__reservationCard__description}>
+                <div className={classes.container__reservationCard__description__title}>
+                  <h1>О номере</h1>
+                </div>
+                <div className={classes.container__reservationCard__description_text}>
+                  В красивом интерьере Семейного люкса мы воплотили атмосферу домашнего уюта, сохранив
+                  функциональность и комфорт премиального гостиничного номера. Современный дизайн спальни
+                  и гостиной продуман в деталях — здесь все необходимое для общения и уединенного отдыха с
+                  семьей. Фантастические панорамы природы из окон вашего номера, утонченный сервис и
+                  невероятная территория комплекса подарят вам незабываемые эмоции от пребывания в отеле
+                  MarT
+                </div>
+              </div>
+            </div>
+            {openModal &&
+                <Modal setOpenModal={setOpenModal}/>
+            }
+          </div>
+        </main>
+        <Footer/>
+      </>
+  );
 };
 
 export default InfoPage;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import classes from './Modal.module.scss'
+import Input from "../UI/Input/Input";
+import Button from '../UI/Button/Button'
+
+const Modal = ({setOpenModal}) => {
+
+  const closeModal = () => setOpenModal(false)
+
+  return (
+      <div className={classes.background} onClick={closeModal}>
+        <div className={classes.background__card} onClick={e => e.stopPropagation()}>
+          <form className={classes.background__card__form}>
+            <h2 className={classes.background__card_title}>Регистрация номера</h2>
+            <span>Номер комнаты: № </span>
+            <Input title="Дата и время прибытия:" name="datetime" type="datetime-local"/>
+            <Input title="ФИО:" name="text" type="text" placeholder={'Введите ФИО'}/>
+            <Input title="Паспорт, серия и номер:" name="number" type="number"
+                   placeholder={'Введите паспортные данные (без пробела)'}/>
+            <Input title="Дата рождения:" name="number" type="number" placeholder={'Введите дату рождения'}/>
+            <div className={classes.background__card__form_radio}>
+              <p>Пол:</p>
+              <span>М</span>
+              <Input type={"radio"} name={'radio'}/>
+              <span>Ж</span>
+              <Input type={"radio"} name={'radio'}/>
+            </div>
+            <Input title="Домашний адрес:" name="text" type="text" placeholder={'Введите домашний адрес'}/>
+            <Input title="Номер телефона:" name="number" type="number" placeholder={'Введите номер телефона'}/>
+            <Button type={'submit'}/>
+
+          </form>
+        </div>
+      </div>
+  );
+};
+
+export default Modal;
