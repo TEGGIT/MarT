3a28671bfbb40e578fd1e45ae115cf32ff232263
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-03-27 00:50:37 +0300

    add email for validation

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Input.module.scss'
 
-const Input = ({title, name, type, placeholder}) => {
+const Input = ({title, name, type, placeholder, onBlur, value,onChange, disabled}) => {
+
   return (
+
       <label className={classes.label}>
         {title}
-        <input className={classes.label__input} name={name} type={type} placeholder={placeholder}/>
+        <input className={classes.label__input} name={name} type={type} placeholder={placeholder} onBlur={onBlur} value={value} onChange={onChange} disabled={disabled}/>
       </label>
   );
 };
