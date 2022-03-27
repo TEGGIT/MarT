a9fd833d385731d81655b78020fdb6f6f813335c
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-03-27 17:36:59 +0300

    add FIO for validation

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -13,22 +13,37 @@ const Modal = ({setOpenModal}) => {
   const [email, setEmail] = useState("")
   const [visidEmail, setVisidEmail] = useState(false)
   const [emailError, setEmailError] = useState("Email не должен быть пустым")
+  const [fio, setFio] = useState("")
+  const [visidFio, setVisidFio] = useState(false)
+  const [fioError, setFioError] = useState("Заполните ФИО")
   const emailValue = (e) => {
     setEmail(e.target.value)
     const re = /\S+@\S+\.\S+/;
-
     if(!re.test(e.target.value)) {
        setEmailError('Неккоретный email')
     }else{
       setEmailError('')
     }
   }
-  const emailBlur = (e) => {
+
+  const fioValue = (e) => {
+    setFio(e.target.value)
+    if (e.target.value > 3 ){
+      setFioError(' Неправильно введены ФИО')
+    }else{
+      setFioError('')
+    }
+  }
+
+  const inputBlur = (e) => {
    switch (e.target.name){
      case 'email':
        setVisidEmail(true)
-         console.log(1)
        break
+     case 'text':
+       setVisidFio(true)
+       break
+
    }
   }
 
@@ -39,9 +54,9 @@ const Modal = ({setOpenModal}) => {
             <h2 className={classes.background__card_title}>Регистрация номера</h2>
             <span>Номер комнаты: № <span/> </span>
             <Input title="Дата и время прибытия:" name="datetime" type="datetime-local"/>
-            <Input title="ФИО:" name="text" type="text" placeholder={'Введите ФИО'} />
-
-            <Input  onChange={e => emailValue(e)}  onBlur={e => emailBlur(e)} title="Адрес электронной почты:" name="email" type="email"
+            <Input title="ФИО:" name="text" type="text" placeholder={'Введите ФИО'}  onBlur={e => inputBlur(e)} onChange={e => fioValue(e)} value={fio}/>
+            {(visidFio && fioError) && <div style={{color:("red")}}>{fioError}</div>}
+            <Input  onChange={e => emailValue(e)}  onBlur={e => inputBlur(e)} title="Адрес электронной почты:" name="email" type="email"
                    placeholder={'Адрес электронной почты'} value={email}/>
             {(visidEmail && emailError) && <div style={{color:("red")}}>{emailError}</div>}
 
