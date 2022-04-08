cdba9403c9825dc6c4cdc489168e8af8121a853b
Fron <syper.martunenko200@yandex.ru>
2022-04-08 16:19:57 +0300

    Created a burger menu for the home page + made a mobile adaptation as well for the front page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,58 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {useState} from 'react'
+import {NavLink} from 'react-router-dom'
 import classes from './Header.module.scss'
 import logo from '../../assets/MarT.svg'
 import iconN from '../../assets/nomera.svg'
 import iconA from '../../assets/Uslugi.svg'
 import iconP from '../../assets/Phone.svg'
 import account from '../../assets/account.svg'
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 
-const Header = () => (
-    <header className={classes.header}>
-      <nav className={classes.header__nav}>
-        <NavLink to="/main-page" className={classes.header__nav_link}>
-          <img src={logo} alt={logo} />
-        </NavLink>
-        <div className={classes.header__nav__btns__container}>
-          <NavLink to="/numbers-page" className={classes.header__nav_link}>
-            <div className={classes.header__nav__btns__container_gap}>
-              <img src={iconN} alt="номера" />
-              <p>Номера</p>
-            </div>
+const Header = () => {
+
+  const [menuActive, setMenuActive] = useState(false)
+
+  return (
+      <header className={classes.header}>
+        <nav className={classes.header__nav}>
+          <NavLink to="/main-page" className={classes.header__nav_link}>
+            <img src={logo} alt={logo}/>
           </NavLink>
-          <NavLink to="/sosi" className={classes.header__nav_link}>
-            <div className={classes.header__nav__btns__container_gap}>
-              <img src={iconA} alt="Услуги" />
-              <p>Услуги</p>
-            </div>
-          </NavLink>
-          <NavLink to="/contact-page" className={classes.header__nav_link}>
-            <div className={classes.header__nav__btns__container_gap}>
-              <img src={iconP} alt="Контакты" />
-              <p>Контакты</p>
-            </div>
-          </NavLink>
-          <NavLink to="/registration-page" className={classes.header__nav_link}>
-            <div className={classes.header__nav__btns__container_gap}>
-              <img src={account} alt="Регистрация" />
-              <p>Регистрация</p>
-            </div>
-          </NavLink>
-        </div>
-      </nav>
-    </header>
+          <div className={classes.header__nav__btns__container}>
+            <NavLink to="/numbers-page" className={classes.header__nav_link}>
+              <div className={classes.header__nav__btns__container_gap}>
+                <img src={iconN} alt="номера" />
+                <p>Номера</p>
+              </div>
+            </NavLink>
+            <NavLink to="/sosi" className={classes.header__nav_link}>
+              <div className={classes.header__nav__btns__container_gap}>
+                <img src={iconA} alt="Услуги" />
+                <p>Услуги</p>
+              </div>
+            </NavLink>
+            <NavLink to="/contact-page" className={classes.header__nav_link}>
+              <div className={classes.header__nav__btns__container_gap}>
+                <img src={iconP} alt="Контакты" />
+                <p>Контакты</p>
+              </div>
+            </NavLink>
+            <NavLink to="/registration-page" className={classes.header__nav_link}>
+              <div className={classes.header__nav__btns__container_gap}>
+                <img src={account} alt="Регистрация" />
+                <p>Регистрация</p>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className={classes.header__nav__burgerBtn} onClick={() => setMenuActive(!menuActive)}>
+            <span/>
+          </div>
+        </nav>
+        <HamburgerMenu active={menuActive} setActive={setMenuActive} header='Меню навигации'/>
+      </header>
   )
+}
+
 
 export default Header
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -9,61 +9,64 @@ import image3 from '../../assets/mainImage3.png'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 
+
 const MainContent = () => (
+
+
     <>
-      <Header />
+      <Header/>
       <main className={classes.main}>
         <section className={classes.main__section}>
           <div
-            className={classes.main__section__contentTop}
-            style={{ backgroundImage: `url(${mainFon})` }}
+              className={classes.main__section__contentTop}
+              style={{backgroundImage: `url(${mainFon})`}}
           >
             <div className={classes.main__section__contentTop__present}>
-              <img src={logoCont} alt="Логотип" />
+              <img src={logoCont} alt="Логотип"/>
               <div className={classes.main__section__contentTop__present__text}>
                 <p>
                   Our priority is{' '}
                   <span
-                    className={
-                      classes.main__section__contentTop__present__text_green
-                    }
+                      className={
+                        classes.main__section__contentTop__present__text_green
+                      }
                   >
                     comfort
                   </span>
                 </p>
               </div>
-              <img src={star} alt="звезды" style={{ marginTop: '1.188rem' }} />
+              <img src={star} alt="звезды" style={{marginTop: '1.188rem'}}/>
             </div>
           </div>
           <div className={classes.main__section__content}>
             <div className={classes.main__section__content__container}>
               <div className={classes.main__section__content__container__text}>
                 <p
-                  className={
-                    classes.main__section__content__container__text_top
-                  }
+                    className={
+                      classes.main__section__content__container__text_top
+                    }
                 >
                   Гостиница «‎
                   <span
-                    className={
-                      classes.main__section__content__container__text_top_color
-                    }
+                      className={
+                        classes.main__section__content__container__text_top_color
+                      }
                   >
                     MarT
                   </span>
                   »
                 </p>
                 <p
-                  className={
-                    classes.main__section__content__container__text_top
-                  }
+                    className={
+                      classes.main__section__content__container__text_top
+                    }
                 >
                   Г. РОСТОВ-НА-ДОНУ
                 </p>
                 <p
-                  className={
-                    classes.main__section__content__container__text_main
-                  }
+                    className={
+                      classes.main__section__content__container__text_main
+                    }
                 >
                   Расположившись с самом сердце Ростова, Гостиница «MarT»
                   окружена очарованием истории и достопримечательностями
@@ -73,15 +76,15 @@ const MainContent = () => (
                   современности. Красивый фасад, 10 комфортных номера - все это
                   сделало Гостиница «MarT» одним из самых привлекательных
                   гостиниц Ростова.
-                  <br />
-                  <br />
+                  <br/>
+                  <br/>
                   В шаговой доступности станции Главный вокзал и парк Горького,
                   а также река Дон.
-                  <br />
-                  <br />
+                  <br/>
+                  <br/>
                   «Почему мы лучшие?»
-                  <br />
-                  <br />
+                  <br/>
+                  <br/>
                   -Мы считаем нашей отличительной особенностью внимательный
                   высокопрофессиональный персонал и отношение к нашим гостям. Мы
                   уделяем максимум внимания каждому гостю и его индивидуальным
@@ -92,17 +95,17 @@ const MainContent = () => (
                   срок. Мы очень гордимся нашим уровнем гостеприимства!
                 </p>
               </div>
-              <div className={classes.main__section__content__image}>
-                <img src={image1} alt="отель1" />
-                <img src={image2} alt="отель2" />
-                <img src={image3} alt="отель3" />
+              <div className={classes.main__section__image}>
+                <img src={image1} alt="отель1"/>
+                <img src={image2} alt="отель2"/>
+                <img src={image3} alt="отель3"/>
               </div>
             </div>
           </div>
         </section>
       </main>
-      <Footer />
+      <Footer/>
     </>
-  )
+)
 
 export default MainContent
